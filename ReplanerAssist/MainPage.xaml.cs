e20f59ef8736e88bfbfa12d82e2a5ee7303f1df3
Alwin Siemens <alwin.siemens@stud.th-owl.de>
2024-05-10 16:54:45 +0200

    MAUI Blazor Hybrid App init

---
+++ ReplanerAssist/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ReplanerAssist;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ReplanerAssist/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ReplanerAssist;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
