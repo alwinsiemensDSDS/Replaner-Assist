dffdcbfed42f71632c23d5c362c439b1ff1d8a29
Johannes-Just <johannes.just@stud.th-owl.de>
2024-06-21 12:53:32 +0200

    person.cs

---
+++ ReplanerAssist/entities/person.cs
@@ -0,0 +1,10 @@
+namespace database
+{
+    public class Person
+    {
+        public int PID { get; set;}
+        public string? Vorname { get; set; }
+        public string? Nachname { get; set; }
+        public string? Notiz { get; set;}
+    }
+}
